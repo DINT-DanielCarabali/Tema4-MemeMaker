d96131d3da7e82af5540ac64e68f492b256c249a
DANIEL STIVEN CARABALI ORDÓÑEZ <dancarord@alu.edu.gva.es>
2022-10-28 18:40:08 +0200

    Agregar archivos de proyecto.

---
+++ MemeMaker/BordesConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class BordesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return new Thickness(2);
+            else
+                return new Thickness(0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
